f4090902a69974174a43d7c84358ea08278332c5
jonatassalgado <jonataseduardosalgado@gmail.com>
2019-02-11 20:25:09 -0200

    Refactor in skills of bot EME

--- bot/eme/skills/_connection_events.js
+++ bot/eme/skills/_connection_events.js
@@ -2,6 +2,18 @@
 
 module.exports = function(controller) {
 
+  var patterns = {
+    positive: /iria|sim|claro|j(a|á)|l(a|á)|partiu|aham|foi/gi,
+    negative: /não|nunca|nem/gi,
+    neutral: /talvez|quiça|quem|sabe|se|p(a|á)/gi
+  }
+
+  var typing = {
+    slow: 100,
+    normal: 100,
+    fast: 100
+  }
+
   var events = [
     {
       "id": 0,
@@ -23,7 +35,7 @@ module.exports = function(controller) {
     },
     {
       "id": 3,
-      "name": "<b>Bike Tour A Magia das Missões</b>",
+      "name": "<b>Bike Tour nas Ruínas das Missões</b>",
       "imageUrl": "./bike-tour-missoes.jpeg",
       "description": "Cicloturismo em São Miguel das Missões e depois do pedal vamos relaxar na acolhedora Pousada das Missões."
     },
@@ -43,7 +55,7 @@ module.exports = function(controller) {
       "id": 6,
       "name": "<b>Feira do Aeromovel</b>",
       "imageUrl": "./feira-do-aeromovel.jpeg",
-      "description": "A feira acontece de frente para a Orla do Guaíba, na Praça do antigo Aeromovel. Um público lindo ocupando a Praça, expositores de marcas locais, gastronomia, cultura, arte e cidadania."
+      "description": "A feira acontece de frente para a Orla do Guaíba, na praça do antigo Aeromovel. Um público lindo ocupando a praça, expositores de marcas locais, gastronomia, cultura, arte e cidadania."
     }
   ]
 
@@ -68,8 +80,12 @@ module.exports = function(controller) {
       },
       [
         {
-          pattern: 'Sim',
+          pattern: /sim|eu|yo|aqui/gi,
           callback: function(res, convo) {
+            if (convo.vars.personaSuitability == undefined){
+              convo.setVar('personaSuitability', [])
+            }
+
             convo.gotoThread('self_presentation')
             convo.next()
           }
@@ -98,7 +114,7 @@ module.exports = function(controller) {
       },
       [
         {
-          pattern: 'Ok',
+          pattern: /ok|sim|claro|vamos|pode/gi,
           callback: function(res, convo) {
             convo.gotoThread('how_works')
             convo.next()
@@ -115,18 +131,18 @@ module.exports = function(controller) {
 
       convo.addMessage({
         text: 'Com base no seu gosto por alguns eventos, <b>vou definir sua persona</b>, o qual me ajudará a indicar eventos futuros',
-        typingDelay: 5000
+        typingDelay: typing.slow
       }, 'how_works')
 
       convo.addMessage({
         text: 'Vamos ao primeiro evento',
-        typingDelay: 5500
+        typingDelay: typing.slow
       }, 'how_works')
 
 
       convo.addMessage({
         text: events[0].name,
-        typingDelay: 5000,
+        typingDelay: typing.slow,
         files: [
           {
             url: events[0].imageUrl,
@@ -137,15 +153,15 @@ module.exports = function(controller) {
 
       convo.addMessage({
         text: events[0].description,
-        action: 'goOrNotGo'
+        typingDelay: typing.normal,
+        action: 'pitchArduino'
       }, 'how_works')
 
 
-
-      // GO OR NOT GO
+      // 0
       convo.addQuestion({
         text: '<b>Você iria neste evento?</b>',
-        typingDelay: 3000,
+        typingDelay: typing.normal,
         quick_replies: [
           {
             title: 'Iria certo',
@@ -162,46 +178,67 @@ module.exports = function(controller) {
         ]
       },[
         {
-          pattern: 'Iria',
+          pattern: patterns.positive,
           callback: function(res, convo) {
-            if (convo.vars.personaSuitability == undefined){
-              convo.setVar('personaSuitability', [])
-            }
-
-            convo.vars.personaSuitability[0] = 1;
-
+            convo.vars.personaSuitability[0] = "1";
             pitchHot(res, convo)
-            
-            console.log(convo.vars);
+            convo.next()
           }
         },
         {
-          pattern: 'talvez',
+          pattern: patterns.neutral,
           callback: function(res, convo) {
-
+            convo.vars.personaSuitability[0] = "0";
+            pitchHot(res, convo)
+            convo.next();
           }
         },
         {
-          pattern: 'Não',
+          pattern: patterns.negative,
           callback: function(res, convo) {
-
+            convo.vars.personaSuitability[0] = "-1";
+            pitchHot(res, convo)
+            convo.next();
           }
         },
         {
           default: true,
           callback: function(res, convo) {
-            // convo.gotoThread('end');
+            bot.reply(convo.source_message, 'Eita! Perguntei se você gostaria de ir no evento acima, é só responder <b>sim</b>, <b>talvez</b> ou <b>não</b>');
+            convo.silentRepeat();
           }
         }
       ],
-      { eventId: 0 }, 'goOrNotGo');
+      {}, 'pitchArduino');
 
 
 
+      // 1
       const pitchHot = function(res, convo){
+        convo.say({
+          text: 'Legal! E neste evento',
+          typingDelay: typing.fast,
+        })
+
+        convo.say({
+          text: events[1].name,
+          typingDelay: typing.normal,
+          files: [
+            {
+              url: events[1].imageUrl,
+              image: true
+            }
+          ]
+        })
+
+        convo.say({
+          text: events[1].description,
+          typingDelay: typing.normal
+        })
+
         convo.ask({
-          text: '<b>Você iria neste evento?</b>',
-          typingDelay: 3000,
+          text: '<b>Você iria?</b>',
+          typingDelay: typing.normal,
           quick_replies: [
             {
               title: 'Iria certo',
@@ -218,50 +255,106 @@ module.exports = function(controller) {
           ]
         },[
           {
-            pattern: 'Iria',
+            pattern: patterns.positive,
             callback: function(res, convo) {
-              if (convo.vars.personaSuitability == undefined){
-                convo.setVar('personaSuitability', [])
-              }
-
-              convo.vars.personaSuitability[1] = 1;
-
-              convo.say({
-                text: events[1].name,
-                typingDelay: 4000,
-                files: [
-                  {
-                    url: events[1].imageUrl,
-                    image: true
-                  }
-                ]
-              })
-              convo.say({
-                text: events[1].description
-              })
-
+              convo.vars.personaSuitability[1] = "1";
               pitchMindfullness(res, convo)
               convo.next();
-
-              console.log(convo.vars);
             }
           },
           {
-            pattern: 'talvez',
+            pattern: patterns.neutral,
             callback: function(res, convo) {
-
+              convo.vars.personaSuitability[1] = "0";
+              pitchMindfullness(res, convo)
+              convo.next();
             }
           },
           {
-            pattern: 'Não',
+            pattern: patterns.negative,
             callback: function(res, convo) {
-
+              convo.vars.personaSuitability[1] = "-1";
+              pitchMindfullness(res, convo)
+              convo.next();
             }
           },
           {
             default: true,
             callback: function(res, convo) {
-              // convo.gotoThread('end');
+              bot.reply(convo.source_message, 'Eita! Perguntei se você gostaria de ir no evento acima, é só responder <b>sim</b>, <b>talvez</b> ou <b>não</b>');
+              convo.silentRepeat();
+            }
+          }
+        ]);
+      }
+
+
+      // 2
+      const pitchMindfullness = function(res, convo){
+        convo.say({
+          text: events[2].name,
+          typingDelay: typing.normal,
+          files: [
+            {
+              url: events[2].imageUrl,
+              image: true
+            }
+          ]
+        })
+
+        convo.say({
+          text: events[2].description,
+          typingDelay: typing.normal
+        })
+
+
+        convo.ask({
+          text: '<b>Você iria neste evento?</b>',
+          typingDelay: typing.normal,
+          quick_replies: [
+            {
+              title: 'Iria certo',
+              payload: 'Iria certo',
+            },
+            {
+              title: 'Talvez',
+              payload: 'Talvez',
+            },
+            {
+              title: 'Não me vejo indo',
+              payload: 'Não iria',
+            },
+          ]
+        },[
+          {
+            pattern: patterns.positive,
+            callback: function(res, convo) {
+              convo.vars.personaSuitability[2] = "1";
+              pitchBikeTour(res, convo);
+              convo.next();
+            }
+          },
+          {
+            pattern: patterns.neutral,
+            callback: function(res, convo) {
+              convo.vars.personaSuitability[2] = "0";
+              pitchBikeTour(res, convo);
+              convo.next();
+            }
+          },
+          {
+            pattern: patterns.negative,
+            callback: function(res, convo) {
+              convo.vars.personaSuitability[2] = "-1";
+              pitchBikeTour(res, convo);
+              convo.next();
+            }
+          },
+          {
+            default: true,
+            callback: function(res, convo) {
+              bot.reply(convo.source_message, 'Eita! Perguntei se você gostaria de ir no evento acima, é só responder <b>sim</b>, <b>talvez</b> ou <b>não</b>');
+              convo.silentRepeat();
             }
           }
         ]);
@@ -269,10 +362,29 @@ module.exports = function(controller) {
 
 
 
-      const pitchMindfullness = function(res, convo){
+
+      // 3
+      const pitchBikeTour = function(res, convo){
+        convo.say({
+          text: events[3].name,
+          typingDelay: typing.normal,
+          files: [
+            {
+              url: events[3].imageUrl,
+              image: true
+            }
+          ]
+        })
+
+        convo.say({
+          text: events[3].description,
+          typingDelay: typing.normal
+        })
+
+
         convo.ask({
           text: '<b>Você iria neste evento?</b>',
-          typingDelay: 3000,
+          typingDelay: typing.normal,
           quick_replies: [
             {
               title: 'Iria certo',
@@ -289,48 +401,252 @@ module.exports = function(controller) {
           ]
         },[
           {
-            pattern: 'Iria',
+            pattern: patterns.positive,
             callback: function(res, convo) {
-              if (convo.vars.personaSuitability == undefined){
-                convo.setVar('personaSuitability', [])
-              }
-
-              convo.vars.personaSuitability[2] = 1;
-
-              convo.say({
-                text: events[2].name,
-                typingDelay: 4000,
-                files: [
-                  {
-                    url: events[2].imageUrl,
-                    image: true
-                  }
-                ]
-              })
-              convo.say({
-                text: events[2].description
-              })
+              convo.vars.personaSuitability[3] = "1";
+              pitchCineIbere(res, convo);
               convo.next();
-
-              console.log(convo.vars);
             }
           },
           {
-            pattern: 'talvez',
+            pattern: patterns.neutral,
             callback: function(res, convo) {
-
+              convo.vars.personaSuitability[3] = "0";
+              pitchCineIbere(res, convo);
+              convo.next();
             }
           },
           {
-            pattern: 'Não',
+            pattern: patterns.negative,
             callback: function(res, convo) {
-
+              convo.vars.personaSuitability[3] = "-1";
+              pitchCineIbere(res, convo);
+              convo.next();
             }
           },
           {
             default: true,
             callback: function(res, convo) {
-              // convo.gotoThread('end');
+              bot.reply(convo.source_message, 'Eita! Perguntei se você gostaria de ir no evento acima, é só responder <b>sim</b>, <b>talvez</b> ou <b>não</b>');
+              convo.silentRepeat();
+            }
+          }
+        ]);
+      }
+
+
+
+      // 4
+      const pitchCineIbere = function(res, convo){
+        convo.say({
+          text: events[4].name,
+          typingDelay: typing.normal,
+          files: [
+            {
+              url: events[4].imageUrl,
+              image: true
+            }
+          ]
+        })
+
+        convo.say({
+          text: events[4].description,
+          typingDelay: typing.normal
+        })
+
+
+        convo.ask({
+          text: '<b>Você iria neste evento?</b>',
+          typingDelay: typing.normal,
+          quick_replies: [
+            {
+              title: 'Iria certo',
+              payload: 'Iria certo',
+            },
+            {
+              title: 'Talvez',
+              payload: 'Talvez',
+            },
+            {
+              title: 'Não me vejo indo',
+              payload: 'Não iria',
+            },
+          ]
+        },[
+          {
+            pattern: patterns.positive,
+            callback: function(res, convo) {
+              convo.vars.personaSuitability[4] = "1";
+              pitchFestaDaUva(res, convo);
+              convo.next();
+            }
+          },
+          {
+            pattern: patterns.neutral,
+            callback: function(res, convo) {
+              convo.vars.personaSuitability[4] = "0";
+              pitchFestaDaUva(res, convo);
+              convo.next();
+            }
+          },
+          {
+            pattern: patterns.negative,
+            callback: function(res, convo) {
+              convo.vars.personaSuitability[4] = "-1";
+              pitchFestaDaUva(res, convo);
+              convo.next();
+            }
+          },
+          {
+            default: true,
+            callback: function(res, convo) {
+              bot.reply(convo.source_message, 'Eita! Perguntei se você gostaria de ir no evento acima, é só responder <b>sim</b>, <b>talvez</b> ou <b>não</b>');
+              convo.silentRepeat();
+            }
+          }
+        ]);
+      }
+
+
+
+
+      // 5
+      const pitchFestaDaUva = function(res, convo){
+        convo.say({
+          text: events[5].name,
+          typingDelay: typing.normal,
+          files: [
+            {
+              url: events[5].imageUrl,
+              image: true
+            }
+          ]
+        })
+
+        convo.say({
+          text: events[5].description,
+          typingDelay: typing.normal
+        })
+
+
+        convo.ask({
+          text: '<b>Você iria neste evento?</b>',
+          typingDelay: typing.normal,
+          quick_replies: [
+            {
+              title: 'Iria certo',
+              payload: 'Iria certo',
+            },
+            {
+              title: 'Talvez',
+              payload: 'Talvez',
+            },
+            {
+              title: 'Não me vejo indo',
+              payload: 'Não iria',
+            },
+          ]
+        },[
+          {
+            pattern: patterns.positive,
+            callback: function(res, convo) {
+              convo.vars.personaSuitability[5] = "1";
+              pitchFeiraAeromovel(res, convo);
+              convo.next();
+            }
+          },
+          {
+            pattern: patterns.neutral,
+            callback: function(res, convo) {
+              convo.vars.personaSuitability[5] = "0";
+              pitchFeiraAeromovel(res, convo);
+              convo.next();
+            }
+          },
+          {
+            pattern: patterns.negative,
+            callback: function(res, convo) {
+              convo.vars.personaSuitability[5] = "-1";
+              pitchFeiraAeromovel(res, convo);
+              convo.next();
+            }
+          },
+          {
+            default: true,
+            callback: function(res, convo) {
+              bot.reply(convo.source_message, 'Eita! Perguntei se você gostaria de ir no evento acima, é só responder <b>sim</b>, <b>talvez</b> ou <b>não</b>');
+              convo.silentRepeat();
+            }
+          }
+        ]);
+      }
+
+
+
+
+      // 6
+      const pitchFeiraAeromovel = function(res, convo){
+        convo.say({
+          text: events[6].name,
+          typingDelay: typing.normal,
+          files: [
+            {
+              url: events[6].imageUrl,
+              image: true
+            }
+          ]
+        })
+
+        convo.say({
+          text: events[6].description,
+          typingDelay: typing.normal
+        })
+
+
+        convo.ask({
+          text: '<b>Você iria neste evento?</b>',
+          typingDelay: typing.normal,
+          quick_replies: [
+            {
+              title: 'Iria certo',
+              payload: 'Iria certo',
+            },
+            {
+              title: 'Talvez',
+              payload: 'Talvez',
+            },
+            {
+              title: 'Não me vejo indo',
+              payload: 'Não iria',
+            },
+          ]
+        },[
+          {
+            pattern: patterns.positive,
+            callback: function(res, convo) {
+              convo.vars.personaSuitability[6] = "1";
+              convo.next();
+            }
+          },
+          {
+            pattern: patterns.neutral,
+            callback: function(res, convo) {
+              convo.vars.personaSuitability[6] = "0";
+              convo.next();
+            }
+          },
+          {
+            pattern: patterns.negative,
+            callback: function(res, convo) {
+              convo.vars.personaSuitability[6] = "-1";
+              convo.next();
+            }
+          },
+          {
+            default: true,
+            callback: function(res, convo) {
+              bot.reply(convo.source_message, 'Eita! Perguntei se você gostaria de ir no evento acima, é só responder <b>sim</b>, <b>talvez</b> ou <b>não</b>');
+              convo.silentRepeat();
             }
           }
         ]);
