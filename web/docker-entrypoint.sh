2895480f9d302dd3e30f7723acc1a27c379936ba
jonatassalgado <jonataseduardosalgado@gmail.com>
2019-05-03 01:14:39 -0300

    Adjust in entrypoint

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -7,7 +7,7 @@ set -e
 bundle check || bundle install --binstubs="$BUNDLE_BIN"
 # Ensure all gems installed. Add binstubs to bin which has been added to PATH in Dockerfile.
 
-rake db:setup db:migrate
+rake db:migrate
 
 whenever --update-crontab
 
