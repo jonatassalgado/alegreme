1f1cca1ea09b2165fba163b5a0a443617365768a
jonatassalgado <jonataseduardosalgado@gmail.com>
2019-05-04 14:06:09 -0300

    Adjust in docker for db migrate

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -7,7 +7,7 @@ set -e
 bundle check || bundle install --binstubs="$BUNDLE_BIN"
 # Ensure all gems installed. Add binstubs to bin which has been added to PATH in Dockerfile.
 
-rake db:migrate
+rake db:migrate RAILS_ENV=production
 
 rails runner "Event.reindex"
 
