13d0ada3ed21e4a2dc1f4116e440176b425743a6
jonatassalgado <jonataseduardosalgado@gmail.com>
2019-05-14 13:03:57 -0300

    Add snackbar to notice

--- web/app/javascript/packs/application.js
+++ web/app/javascript/packs/application.js
@@ -10,6 +10,7 @@
 console.log('Hello World from Webpacker')
 
 import 'controllers'
+// import 'default-passive-events'
 import * as mdc from 'material-components-web';
 
 import {MDCTopAppBar} from '@material/top-app-bar/index';
@@ -20,42 +21,14 @@ import {MDCSelect} from '@material/select';
 import {MDCFormField} from '@material/form-field';
 import {MDCCheckbox} from '@material/checkbox';
 import {MDCMenu} from '@material/menu';
-
-
-// Turbolinks.scroll = {};
-
-// document.addEventListener("turbolinks:load", ()=> {
-  
-//   const elements = document.querySelectorAll("[data-turbolinks-scroll]");
-
-//   elements.forEach(function(element){
-    
-//     element.addEventListener("click", ()=> {
-//       Turbolinks.scroll['top'] = document.scrollingElement.scrollTop;
-//     });
-    
-//     element.addEventListener("submit", ()=> {
-//       Turbolinks.scroll['top'] = document.scrollingElement.scrollTop;
-//     });
-    
-//   });
-  
-//   if (Turbolinks.scroll['top']) {
-//     document.scrollingElement.scrollTo(0, Turbolinks.scroll['top']);
-//   }
-
-//   Turbolinks.scroll = {};
-// });
+import {MDCSnackbar} from '@material/snackbar';
 
 
 document.addEventListener("DOMContentLoaded", function() {
+  
   // TopAppBar
   const topAppBarElement = document.querySelector('.mdc-top-app-bar');
   const topAppBar = new MDCTopAppBar(topAppBarElement);
-
-  // List
-  // const list = MDCList.attachTo(document.querySelector('.mdc-list'));
-  // list.wrapFocus = true;
 
   // Button
   const buttons = document.querySelectorAll('.mdc-button');
@@ -90,6 +63,14 @@ document.addEventListener("DOMContentLoaded", function() {
       new MDCTextField(field);
     };
   })
+
+
+  // Snackbar
+  const snackbarElem = document.querySelector('.mdc-snackbar');
+  if (snackbarElem) {
+    const snackbar = new MDCSnackbar(snackbarElem);
+    snackbar.open();
+  }
 
 
   // Select
