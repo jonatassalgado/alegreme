39a4da6cec3fed3955db2cf180423325720d881e
jonatassalgado <jonataseduardosalgado@gmail.com>
2019-08-19 22:46:38 -0300

    Adjust whenever to debug

--- web/config/schedule.rb
+++ web/config/schedule.rb
@@ -8,18 +8,19 @@
 
 ENV.each { |k, v| env(k, v) }
 
-set :output, "/var/www/alegreme/log/cron_log.log"
+set :output, "/var/www/alegreme/log/cron.log"
 
 every 1.hour do
-  command "echo 'Tá funcionando'"
+  command '/bin/echo "hello"'
 end
-#
-every 1.day, at: '15:00 pm' do
+
+every 6.hour do
   rake "db:dump"
 end
 
-every 1.day, at: '15:30 am' do
+every 1.day, at: '11:00 pm' do
   rake "populate:facebook similar:events"
 end
+
 
 # Learn more: http://github.com/javan/whenever
