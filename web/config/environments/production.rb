ad3dd85c079408b474d6d357e25b6c2eb1a6ca6e
jonatassalgado <jonataseduardosalgado@gmail.com>
2019-09-19 09:54:46 -0300

    Add config to mailer load images in production

--- web/config/environments/production.rb
+++ web/config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "https://alegreme.com" }
-
+  config.action_mailer.asset_host = "https://alegreme.com"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
