04b9d43a69be9f9a61385f4bbe141074f582dc59
jonatassalgado <jonataseduardosalgado@gmail.com>
2019-09-19 09:37:38 -0300

    Fix mailer syntax

--- web/app/mailers/invite_mailer.rb
+++ web/app/mailers/invite_mailer.rb
@@ -1,5 +1,5 @@
 class InviteMailer < ApplicationMailer
-	default :from => 'Alegreme 🙂', :reply_to => 'oi@alegreme.com'
+	default :from => 'Alegreme', :reply_to => 'oi@alegreme.com'
 
 	# send a signup email to the user, pass in the user object that   contains the user's email address
 	def send_request_invite_confirmation
