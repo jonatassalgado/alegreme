30ece43182d604139f4917c4c26759da5cc78658
Jonatas Eduardo <jonataseduardosalgado@gmail.com>
2019-10-05 18:15:18 -0300

    Adjust puma configs

--- web/config/puma/puma-development.rb
+++ web/config/puma/puma-development.rb
@@ -14,6 +14,9 @@ port        ENV.fetch("PORT") { 3000 }
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 2 }
+threads 1, threads_count
+
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
