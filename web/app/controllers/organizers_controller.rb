9118ac1e1e0f85ec80c628dfb406ffc36af1a4d2
jonatassalgado <jonataseduardosalgado@gmail.com>
2019-10-06 13:04:49 -0300

    Filter high scores events in siilar, organizer and place pages

--- web/app/controllers/organizers_controller.rb
+++ web/app/controllers/organizers_controller.rb
@@ -24,7 +24,7 @@ class OrganizersController < ApplicationController
 		# 	end
 		# 	format.html do
 				# Rails.cache.fetch("#{current_or_guest_user}_user_personas", expires_in: 1.hour) do
-				events      = @organizer.events.active
+				events      = @organizer.events.active.with_high_score
 				@collection = EventServices::CollectionCreator.new(current_user, params).call(events, organizers: [params[:id]], limit: 20)
 
 				@locals = mount_section_attrs
--- web/app/controllers/places_controller.rb
+++ web/app/controllers/places_controller.rb
@@ -23,7 +23,7 @@ class PlacesController < ApplicationController
 			# end
 			# format.html do
 				# Rails.cache.fetch("#{current_or_guest_user}_user_personas", expires_in: 1.hour) do
-				events      = @place.events.active
+				events      = @place.events.active.with_high_score
 				@collection = EventServices::CollectionCreator.new(current_user, params).call(events, places: [params[:id]])
 
 		@locals = mount_section_attrs
