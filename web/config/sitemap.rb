616544fed8a36d26117ea3f327487e539340b00d
jonatassalgado <jonataseduardosalgado@gmail.com>
2019-10-13 23:56:41 -0300

    Adjust modified_at attribute in sitemap

--- web/config/sitemap.rb
+++ web/config/sitemap.rb
@@ -1,5 +1,6 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://www.alegreme.com"
+SitemapGenerator::Sitemap.verbose = true
 
 SitemapGenerator::Sitemap.create do
 	# Put links creation logic here.
@@ -22,16 +23,16 @@ SitemapGenerator::Sitemap.create do
 	# Add all articles:
 	#
 	Event.find_each do |event|
-		add event_path(event), :lastmod => event.updated_at, :changefreq => 'daily', :priority => 0.9
+		add event_path(event), :lastmod => DateTime.now, :changefreq => 'daily', :priority => 0.9
 	end
 
 	Place.find_each do |place|
-		add place_path(place), :lastmod => place.updated_at, :changefreq => 'weekly', :priority => 0.8
+		add place_path(place), :lastmod => DateTime.now, :changefreq => 'daily', :priority => 0.8
 	end
 
 	Organizer.find_each do |organizer|
-		add organizer_path(organizer), :lastmod => organizer.updated_at, :changefreq => 'weekly', :priority => 0.8
+		add organizer_path(organizer), :lastmod => DateTime.now, :changefreq => 'daily', :priority => 0.8
 	end
 
-	add today_events_path, :changefreq => 'daily', :priority => 0.9
+	add today_events_path, :lastmod => DateTime.now, :changefreq => 'daily', :priority => 0.9
 end
