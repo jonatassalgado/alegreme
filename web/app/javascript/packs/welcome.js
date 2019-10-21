d240b456fda42e6dd8434a3dfae28cf1ae46dec7
jonatassalgado <jonataseduardosalgado@gmail.com>
2019-10-21 14:02:30 -0300

    Fix in request pwa installer

--- web/app/javascript/packs/welcome.js
+++ web/app/javascript/packs/welcome.js
@@ -1,7 +1,7 @@
 import {MDCSnackbar}  from '@material/snackbar';
 import {MDCTextField} from '@material/textfield';
 
-import pwaInstallPrompt from "pwa-install-prompt";
+// import pwaInstallPrompt from "pwa-install-prompt";
 
 document.addEventListener('DOMContentLoaded', () => {
 	const textFieldEls = document.querySelectorAll('.mdc-text-field');
@@ -10,27 +10,27 @@ document.addEventListener('DOMContentLoaded', () => {
         new MDCTextField(textFieldEl);
     });
 
-	window.prompt = new pwaInstallPrompt(".pwa-install-prompt__container", {
-		active_class: "is-active",
-		closer      : ".pwa-install-prompt__overlay",
-		condition   : false,
-		expires     : 180,
-		show_after  : 90,
-		on          : {
-			beforeOpen : function () {
-				// console.log("before open!");
-			},
-			afterOpen  : function () {
-				// console.log("after open!");
-			},
-			beforeClose: function () {
-				// console.log("before close!");
-			},
-			afterClose : function () {
-				// console.log("after close!");
-			},
-		}
-	});
+	// window.prompt = new pwaInstallPrompt(".pwa-install-prompt__container", {
+	// 	active_class: "is-active",
+	// 	closer      : ".pwa-install-prompt__overlay",
+	// 	condition   : false,
+	// 	expires     : 180,
+	// 	show_after  : 90,
+	// 	on          : {
+	// 		beforeOpen : function () {
+	// 			// console.log("before open!");
+	// 		},
+	// 		afterOpen  : function () {
+	// 			// console.log("after open!");
+	// 		},
+	// 		beforeClose: function () {
+	// 			// console.log("before close!");
+	// 		},
+	// 		afterClose : function () {
+	// 			// console.log("after close!");
+	// 		},
+	// 	}
+	// });
 
 
 	const snackbarEl = document.querySelector('.mdc-snackbar');
