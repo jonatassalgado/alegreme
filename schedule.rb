0947abf383c73293fae5007835e6370dc664a568
Jonatas Eduardo <jonataseduardosalgado@gmail.com>
2019-12-11 21:37:08 -0300

    Update in schedule tasks

--- schedule.rb
+++ schedule.rb
@@ -1,15 +1,15 @@
 set :output, '/root/alegreme/logs/whenever/cron.log'
 
-every 4.hours do
+every 6.hours do
   command "cd /root/alegreme && sudo docker-compose run --rm scrapy scrapy crawl event -s JOBDIR=crawls/event-1 && sudo docker-compose restart splash"
 end
 
-every 3.hours do
+every 5.hours do
   command "cd /root/alegreme && sudo docker exec alegreme_app_1 rake populate:events similar:events suggestions:users sitemap:refresh search:refresh"
 end
 
 every 8.hours do
-  command "cd /root/alegreme && sudo docker-compose run --rm scrapy scrapy crawl movie -s JOBDIR=crawls/movie-1"
+  command "cd /root/alegreme && sudo docker-compose run --rm scrapy scrapy crawl movie -s JOBDIR=crawls/movie-1 && sudo docker-compose restart splash"
 end
 
 every 7.hours do
