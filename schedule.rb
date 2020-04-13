6b4d9bd32201070b338827913a740dc23fcc3db0
Jonatas Eduardo <jonataseduardosalgado@gmail.com>
2020-04-13 13:51:26 -0300

    Increase time of scrapy jobs

--- schedule.rb
+++ schedule.rb
@@ -1,21 +1,21 @@
 set :output, '/root/alegreme/logs/whenever/cron.log'
 
-every 6.hours do
+every 24.hours do
   command "cd /root/alegreme && sudo docker-compose run --rm scrapy scrapy crawl event -s JOBDIR=crawls/event-1 && sudo docker-compose restart splash"
 end
 
-every 5.hours do
+every 23.hours do
   command "cd /root/alegreme && sudo docker exec alegreme_app_1 rake populate:events similar:events suggestions:users sitemap:refresh search:refresh"
 end
 
-every 8.hours do
+every 24.hours do
   command "cd /root/alegreme && sudo docker-compose run --rm scrapy scrapy crawl movie -s JOBDIR=crawls/movie-1 && sudo docker-compose restart splash"
 end
 
-every 7.hours do
+every 23.hours do
   command "cd /root/alegreme && sudo docker exec alegreme_app_1 rake populate:movies"
 end
 
-every 1.day do
+every 2.days do
   command "cd /root/alegreme && > logs/api/error.log"
 end
