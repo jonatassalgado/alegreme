29dd2f8ef6110a099d1fbcb98a5a5908ca79681a
Jonatas Eduardo <jonataseduardosalgado@gmail.com>
2020-05-16 15:36:45 -0300

    Adjust original image size

--- web/app/uploaders/event_image_uploader.rb
+++ web/app/uploaders/event_image_uploader.rb
@@ -16,29 +16,14 @@ class EventImageUploader < Shrine
     palette_color(io.path, 3)
   end
 
-  # def generate_location(io, context)
-  #   filename = context[:metadata]["filename"]
-
-  #   "#{filename}_#{super}"
-  # end
-
   process(:store) do |io, context|
-    # image_optim = ImageOptim.new(pngout: false, svgo: false)
-    # image_optim.optimize_image!(io.download)
-    # io.open
-
-    versions = { 'original': io }
-
     io.download do |original|
       pipeline = ImageProcessing::MiniMagick.source(original)
 
-      versions['feed'] = pipeline.resize_to_fill!(340, 190)
-      # versions['feed-mobile'] = pipeline.resize_to_fill!(183, 103)
-      # versions['single'] = pipeline.resize_to_fill!(600, 338)
-      # versions['single-mobile'] = pipeline.resize_to_fill!(414, 232)
-      # versions['list-mobile'] = pipeline.resize_to_fill!(116, 66)
+      {
+        original: pipeline.resize_to_fill!(850, 475),
+        feed: pipeline.resize_to_fill!(340, 190)
+      }
     end
-
-     versions
   end
 end
