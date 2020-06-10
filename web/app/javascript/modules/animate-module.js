0a30503f5818485f71124af90b72f8d85e5b3a2d
Jonatas Eduardo <jon@warrenbrasil.com.br>
2020-06-10 14:19:28 -0300

    Fix animate module

--- web/app/javascript/modules/animate-module.js
+++ web/app/javascript/modules/animate-module.js
@@ -10,17 +10,18 @@ const AnimateModule = (function () {
 	module.animateOpenPage = () => {
 		const page = document.querySelector(".me-page.is-animated");
 
-		if (isPwa && page) {
+		if (isPwa) {
 			console.log("[ANIMATE]: animate page");
-			page.style.opacity   = 0;
-			page.style.transform = "translate(0, 10vh) scale(0.95)"
 
-			requestAnimationFrame(() => {
-				if (page) {
-					page.style.opacity   = 1;
-					page.style.transform = ""
-				}
-			});
+			if (page) {
+				page.style.opacity   = 0;
+				page.style.transform = "translate(0, 10vh) scale(0.95)"
+
+				requestAnimationFrame(() => {
+						page.style.opacity   = 1;
+						page.style.transform = ""
+				});
+			}
 			document.addEventListener("turbolinks:before-cache", () => {
 				if (page) {
 					page.style.opacity   = 0;
@@ -33,32 +34,33 @@ const AnimateModule = (function () {
 	module.animatePageHide = () => {
 		const page = document.querySelector(".me-page.is-animated");
 
-		if (isPwa && page) {
+		if (isPwa) {
 			console.log("[ANIMATE]: page hide");
-			page.style.opacity   = 1;
-			page.style.transform = ""
 
-			requestAnimationFrame(() => {
-				if (page) {
+			if (page) {
+				page.style.opacity   = 1;
+				page.style.transform = ""
+
+				requestAnimationFrame(() => {
 					page.style.opacity   = 0;
 					page.style.transform = "scale(0.95)"
-				}
-			});
+				});
+			}
 		}
 	};
 
 	module.animateBackbutton = () => {
 		const page = document.querySelector(".me-page.is-animated");
 
-		if (isPwa && page) {
+		if (isPwa) {
 			console.log("[ANIMATE]: backbutton");
 
-			requestAnimationFrame(() => {
-				if (page) {
-					page.style.opacity = 0;
-					page.style.transform = "translate(0, 10vh) scale(0.95)"
-				}
-			});
+			if (page) {
+				requestAnimationFrame(() => {
+						page.style.opacity = 0;
+						page.style.transform = "translate(0, 10vh) scale(0.95)"
+				});
+			}
 
 			setTimeout(() => {
 				window.history.back();
