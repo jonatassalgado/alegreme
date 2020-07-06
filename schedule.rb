97428b721e4c7bd3e8eae555e7fb37412ca3c032
jonatassalgado <jonataseduardosalgado@gmail.com>
2020-07-06 16:21:37 -0300

    Add movies new release to schedule

--- schedule.rb
+++ schedule.rb
@@ -8,6 +8,7 @@ every 23.hours do
   command "cd /root/alegreme && sudo docker exec alegreme_app_1 rake populate:events similar:events suggestions:users sitemap:refresh search:refresh"
 end
 
+
 # every 24.hours do
 #   command "cd /root/alegreme && sudo docker-compose run --rm scrapy scrapy crawl movie -s JOBDIR=crawls/movie-1 && sudo docker-compose restart splash"
 # end
@@ -15,6 +16,10 @@ end
 # every 23.hours do
 #   command "cd /root/alegreme && sudo docker exec alegreme_app_1 rake populate:movies"
 # end
+
+every 1.week do
+  command "cd /root/alegreme && sudo docker exec alegreme_app_1 rake populate:movies:new_release"
+end
 
 every 2.days do
   command "cd /root/alegreme && > logs/api/error.log"
