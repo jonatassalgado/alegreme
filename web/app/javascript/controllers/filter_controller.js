378a8d60ee9389e197927ac56e073e3f7f99ea27
jonatassalgado <jonataseduardosalgado@gmail.com>
2020-07-09 14:36:44 -0300

    Fix filter select to await MDCChip transition

--- web/app/javascript/controllers/filter_controller.js
+++ web/app/javascript/controllers/filter_controller.js
@@ -14,23 +14,11 @@ export default class FilterController extends ApplicationController {
     }
 
     select(event) {
-        const selectedChips = new Promise((resolve, reject) => {
-            const chipValues = this.MDCChipSet.selectedChipIds.map((chipId, i) => {
-                return this.chipsetTarget.querySelector(`#${chipId}`).dataset.filterValue;
-            })
-
-            if (chipValues) {
-                resolve(chipValues)
+        this.stimulate("Event#update_collection", event.target, {
+            filter: {
+                type:  this.data.get("type"),
+                value: event.detail.selected ? [event.target.dataset.filterValue] : []
             }
-        })
-
-        selectedChips.then((selectedValues) => {
-            this.stimulate("Event#update_collection", event.target, {
-                filter: {
-                    type:  this.data.get("type"),
-                    value: selectedValues
-                }
-            })
         })
     }
 
