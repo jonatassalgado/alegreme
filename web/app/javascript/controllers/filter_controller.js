5c523bac7e099ec414980b268102032f93a68cd4
jonatassalgado <jonataseduardosalgado@gmail.com>
2020-07-09 14:52:22 -0300

    Add progressbarmodule to filter_controller.js

--- web/app/javascript/controllers/filter_controller.js
+++ web/app/javascript/controllers/filter_controller.js
@@ -1,5 +1,6 @@
 import ApplicationController from "./application_controller"
 import {MDCChipSet}          from "@material/chips";
+import {ProgressBarModule}   from "../modules/progressbar-module";
 
 export default class FilterController extends ApplicationController {
     static targets = ["chipset", "container", "chip", "input", "inputContainer"];
@@ -14,11 +15,14 @@ export default class FilterController extends ApplicationController {
     }
 
     select(event) {
+        ProgressBarModule.show();
         this.stimulate("Event#update_collection", event.target, {
             filter: {
                 type:  this.data.get("type"),
                 value: event.detail.selected ? [event.target.dataset.filterValue] : []
             }
+        }).then(() => {
+            ProgressBarModule.hide();
         })
     }
 
