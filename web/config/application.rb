e37ace9e73c1b49d15360d2129521b804141f550
jonatassalgado <jonataseduardosalgado@gmail.com>
2020-07-10 18:37:57 -0300

    Revert "Add Rack::Deflater"
    This reverts commit a4a9a572

--- web/config/application.rb
+++ web/config/application.rb
@@ -23,7 +23,6 @@ module App
 		config.filter_parameters << :password
 
 		config.middleware.use Rack::Attack
-		config.middleware.insert_after ActionDispatch::Static, Rack::Deflater
 
 		# Settings in config/environments/* take precedence over those specified here.
 		# Application configuration can go into files in config/initializers
