157bec9b746a652f64924f3f68840b999678facc
jonatassalgado <jonataseduardosalgado@gmail.com>
2020-07-10 14:17:13 -0300

    Send to Sentry errors from application_reflex

--- web/app/reflexes/application_reflex.rb
+++ web/app/reflexes/application_reflex.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationReflex < StimulusReflex::Reflex
-  # Put application wide Reflex behavior in this file.
-  delegate :current_user, to: :connection
-  #
-  # Example:
-  #
-  #   # If your ActionCable connection is: `identified_by :current_user`
-  #   delegate :current_user, to: :connection
-  #
-  # Learn more at: https://docs.stimulusreflex.com
-  #
+	# Put application wide Reflex behavior in this file.
+	delegate :current_user, to: :connection
+
+	rescue_from StandardError do |exception|
+		Raven.capture_exception exception, level: 'warning'
+	end
+	#
+	# Example:
+	#
+	#   # If your ActionCable connection is: `identified_by :current_user`
+	#   delegate :current_user, to: :connection
+	#
+	# Learn more at: https://docs.stimulusreflex.com
+	#
 end
