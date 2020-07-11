0953cc34679420d0eff37fe66c101a6e0d8ebec1
jonatassalgado <jonataseduardosalgado@gmail.com>
2020-07-11 11:00:20 -0300

    Fix in feed controller

--- web/app/controllers/feeds_controller.rb
+++ web/app/controllers/feeds_controller.rb
@@ -15,7 +15,7 @@ class FeedsController < ApplicationController
 				         :user_taste_events_saved => current_user&.taste_events_saved&.size
 		         })
 
-		@saved_events                 ||= current_user&.saved_events&.active&.order_by_date
+		@saved_events                 ||= current_user ? current_user&.saved_events&.active&.order_by_date : Event.none
 		@new_events_today             ||= Event.active.not_in_saved(current_user).where("created_at > ?", DateTime.now - 24.hours).includes(:place)
 		@events_this_week             ||= Event.active.with_high_score.not_in_saved(current_user).in_days((DateTime.now.beginning_of_day.yday..(DateTime.now.beginning_of_day.yday + 8))).includes(:place).order_by_date
 		@events_in_user_suggestions   ||= current_user ? Event.active.not_in_saved(current_user).not_in(@events_this_week.map(&:id)).in_user_suggestions(current_user).includes(:place).order_by_date : Event.none
