f9834dd8407b61c4e54731c45b39cb71387dd0a4
jonatassalgado <jonataseduardosalgado@gmail.com>
2020-08-06 21:27:05 -0300

    Adjust in entrypoint

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -5,6 +5,8 @@ set -e
 # Exit on fail
 
 bundle check || bundle install --binstubs="$BUNDLE_BIN"
+rake db:exists && rake db:migrate
+bundle exec rake search:refresh
 
 exec "$@"
 # Finally call command issued to the docker service
