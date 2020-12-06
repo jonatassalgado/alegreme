62871b3a1cdabe5da0759a30e65232a483e4d3c8
jonatassalgado <jonataseduardosalgado@gmail.com>
2020-12-06 01:07:37 -0300

    Refactor likes size in swipable_component.rb for performance

--- web/app/components/hero/swipable_component.rb
+++ web/app/components/hero/swipable_component.rb
@@ -3,8 +3,8 @@ class Hero::SwipableComponent < ViewComponentReflex::Component
   def initialize(user:, events:)
     @user           = user
     @events         = events
-    @events_visible = events.size
-    @events_liked   = user.liked_events.size
+    @events_visible = @events.size
+    @events_liked   = @user.liked_events.size
   end
 
   def decrease(args = {})
