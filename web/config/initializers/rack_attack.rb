13d0758697b74a047d90b07c1e1e432c950b0dce
jonatassalgado <jonataseduardosalgado@gmail.com>
2020-12-16 20:43:37 -0300

    Add more filters like fail2ban to rack-attack

--- web/config/initializers/rack_attack.rb
+++ web/config/initializers/rack_attack.rb
@@ -74,4 +74,26 @@ class Rack::Attack
 	#    {},   # headers
 	#    ['']] # body
 	# end
+
+
+	# Block suspicious requests for '/etc/password' or wordpress specific paths.
+	# After 3 blocked requests in 10 minutes, block all requests from that IP for 5 minutes.
+	blocklist('fail2ban/malicious-requests') do |req|
+		# `filter` returns truthy value if request fails, or if it's from a previously banned IP
+		# so the request is blocked
+		Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 2, findtime: 1.day, bantime: 1.year) do
+			# The count for the IP is incremented if the return value is truthy
+			CGI.unescape(req.query_string) =~ %r{/etc/passwd} ||
+				req.path.include?('/etc/passwd') ||
+				req.path.include?('wp-admin') ||
+				req.path.include?('wp-login') ||
+				req.path.include?('sql') ||
+				req.path.include?('php') ||
+				req.path.include?('mysql') ||
+				req.path.include?('admin') ||
+				req.path.include?('database') ||
+				req.path.include?('2php') ||
+				req.path.include?('myadmin')
+		end
+	end
 end
