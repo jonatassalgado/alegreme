44f75716d2330a450afa10de4f2015842b9935ed
jonatassalgado <jonataseduardosalgado@gmail.com>
2021-01-17 16:47:32 -0300

    Refactor in docker-compose to use different file for scrapy and refactor user_agents

--- schedule.rb
+++ schedule.rb
@@ -1,27 +1,14 @@
 set :output, '/root/alegreme/logs/whenever/cron.log'
 
-every 30.hours do
-  command "cd /root/alegreme && sudo docker-compose run --rm scrapy scrapy crawl event -s JOBDIR=crawls/event-1 && sudo docker-compose restart splash"
+every 1.day, at: '02:00 am' do
+  command "cd /root/alegreme/scrapy && sudo docker-compose run --rm scrapy scrapy crawl event -s JOBDIR=crawls/event-1 && sudo docker-compose down"
 end
 
-every 23.hours do
+every 1.day, at: '05:30 am' do
   command "cd /root/alegreme && sudo docker exec alegreme_app_1 bundle exec rake populate:events similar:events suggestions:users sitemap:refresh search:refresh"
 end
 
-
-# every 24.hours do
-#   command "cd /root/alegreme && sudo docker-compose run --rm scrapy scrapy crawl movie -s JOBDIR=crawls/movie-1 && sudo docker-compose restart splash"
-# end
-
-# every 23.hours do
-#   command "cd /root/alegreme && sudo docker exec alegreme_app_1 bundle exec rake populate:movies"
-# end
-
-# every 2.weeks do
-#   command "cd /root/alegreme && sudo docker exec alegreme_app_1 bundle exec rake populate:movies:new_release"
-# end
-
-every 2.days do
+every 1.day do
   command "cd /root/alegreme && > logs/api/error.log"
 end
 
--- scrapy/alegreme/spiders/event.py
+++ scrapy/alegreme/spiders/event.py
@@ -4,15 +4,31 @@ import base64
 import json
 import os
 import random
-import shadow_useragent
+# import shadow_useragent
 
 from urllib.parse import urljoin
 from alegreme.items import Event, EventOrganizer, EventOrganizerLoader
 from scrapy_splash import SplashRequest
 from scrapy.loader import ItemLoader
 
-ua = shadow_useragent.ShadowUserAgent()
-ua = ua.firefox
+# ua = shadow_useragent.ShadowUserAgent()
+# ua = ua.firefox
+
+user_agents = [
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15",
+  "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0",
+  "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36"
+]
+
+random.shuffle(user_agents)
+ua = user_agents[0]
 
 parse_event_script = """
     function main(splash, args)
@@ -85,7 +101,7 @@ class EventSpider(scrapy.Spider):
         'ITEM_PIPELINES': {
             'alegreme.pipelines.EventPipeline': 400
         },
-        'CLOSESPIDER_ITEMCOUNT': 120,
+        'CLOSESPIDER_ITEMCOUNT': 60,
         'DEPTH_LIMIT': 4
     }
 
