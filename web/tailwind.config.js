b6521cc5b836d6da21d32b4cf5f966f932e9c365
jonatassalgado <jonataseduardosalgado@gmail.com>
2021-01-17 16:56:42 -0300

    Update tailwindcss to 2.0

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     purge:    {
-        enabled: process.env.RAILS_ENV !== "development",
-        content: [
+        enabled:              process.env.RAILS_ENV !== "development",
+        content:              [
             "./app/views/**/*.html.erb",
             "./app/views/**/**/*.html.erb",
             "./app/components/*.html.erb",
@@ -11,9 +11,74 @@ module.exports = {
             "./app/helpers/**/*.rb",
             "./app/javascript/**/*.js",
             "./app/javascript/**/**/*.js"
-        ]
+        ],
+        preserveHtmlElements: false
     },
     theme:    {
+        colors:                  {
+            transparent: 'transparent',
+            current:     'currentColor',
+            black:       '#000',
+            white:       '#fff',
+            green:       {
+                50:  '#F5FCF6',
+                100: '#ECF8EE',
+                200: '#CEEED4',
+                300: '#B1E3BB',
+                400: '#77CF87',
+                500: '#3CBA54',
+                600: '#36A74C',
+                700: '#247032',
+                800: '#1B5426',
+                900: '#123819',
+            },
+            gray:        {
+                50:  '#F5F5F5',
+                100: '#EAEBEB',
+                200: '#CBCCCE',
+                300: '#ABADB0',
+                400: '#6C7074',
+                500: '#2D3339',
+                600: '#292E33',
+                700: '#1B1F22',
+                800: '#14171A',
+                900: '#0E0F11',
+            },
+            yellow:      {
+                100: '#fffbf0',
+                200: '#ffefc7',
+                300: '#ffe5a3',
+                400: '#ffd97a',
+                500: '#ffcf57',
+                600: '#ffbb0f',
+                700: '#c28b00',
+                800: '#7a5800',
+                900: '#332500'
+            },
+            red:         {
+                50:  '#FDF4F5',
+                100: '#FBE9EC',
+                200: '#F6C9CF',
+                300: '#F0A8B2',
+                400: '#E46778',
+                500: '#D9263E',
+                600: '#C32238',
+                700: '#821725',
+                800: '#62111C',
+                900: '#410B13',
+            },
+            purple:      {
+                100: '#f3f4fc',
+                200: '#d0d1f1',
+                300: '#acafe7',
+                400: '#898ddc',
+                500: '#6167d1',
+                600: '#383ebd',
+                700: '#292d8a',
+                800: '#1b1e5b',
+                900: '#0c0d27'
+            }
+        },
         animations:              {
             "spin": {
                 from: {
@@ -22,21 +87,10 @@ module.exports = {
                 to:   {
                     transform: "rotate(360deg)",
                 },
-            },
-            "jump": {
-                "0%":   {
-                    transform: "translateY(0%)",
-                },
-                "50%":  {
-                    transform: "translateY(-100%)",
-                },
-                "100%": {
-                    transform: "translateY(0%)",
-                },
-            },
+            }
         },
         animationDuration:       { // defaults to these values
-            "default": "1s",
+            "DEFAULT": "1s",
             "0s":      "0s",
             "1s":      "1s",
             "2s":      "2s",
@@ -45,106 +99,38 @@ module.exports = {
             "5s":      "5s",
         },
         animationTimingFunction: {
-            "default":     "ease",
-            "linear":      "linear",
-            "ease":        "ease",
-            "ease-in":     "ease-in",
-            "ease-out":    "ease-out",
-            "ease-in-out": "ease-in-out",
+            "DEFAULT": "ease",
+            "linear":  "linear",
+            "ease":    "ease",
+            "ease-in": "ease-in"
         },
         animationDelay:          {
-            "default": "0s",
+            "DEFAULT": "0s",
             "0s":      "0s",
             "1s":      "1s",
             "2s":      "2s",
-            "3s":      "3s",
-            "4s":      "4s",
-            "5s":      "5s",
+            "3s":      "3s"
         },
         animationIterationCount: {
-            "default":  "infinite",
+            "DEFAULT":  "infinite",
             "once":     "1",
             "infinite": "infinite",
         },
         animationDirection:      {
-            "default":           "normal",
+            "DEFAULT":           "normal",
             "normal":            "normal",
             "reverse":           "reverse",
             "alternate":         "alternate",
             "alternate-reverse": "alternate-reverse",
         },
         animationFillMode:       {
-            "default":   "none",
-            "none":      "none",
-            "forwards":  "forwards",
-            "backwards": "backwards",
-            "both":      "both",
+            "DEFAULT": "none"
         },
         animationPlayState:      {
             "running": "running",
             "paused":  "paused",
         },
         extend:                  {
-            colors:       {
-                "green":  {
-                    "50":  "#F5FCF6",
-                    "100": "#ECF8EE",
-                    "200": "#CEEED4",
-                    "300": "#B1E3BB",
-                    "400": "#77CF87",
-                    "500": "#3CBA54",
-                    "600": "#36A74C",
-                    "700": "#247032",
-                    "800": "#1B5426",
-                    "900": "#123819",
-                },
-                "gray":   {
-                    "50":  "#F5F5F5",
-                    "100": "#EAEBEB",
-                    "200": "#CBCCCE",
-                    "300": "#ABADB0",
-                    "400": "#6C7074",
-                    "500": "#2D3339",
-                    "600": "#292E33",
-                    "700": "#1B1F22",
-                    "800": "#14171A",
-                    "900": "#0E0F11",
-                },
-                "yellow": {
-                    "100": "#fffbf0",
-                    "200": "#ffefc7",
-                    "300": "#ffe5a3",
-                    "400": "#ffd97a",
-                    "500": "#ffcf57",
-                    "600": "#ffbb0f",
-                    "700": "#c28b00",
-                    "800": "#7a5800",
-                    "900": "#332500"
-                },
-                "red":    {
-                    "50":  "#FDF4F5",
-                    "100": "#FBE9EC",
-                    "200": "#F6C9CF",
-                    "300": "#F0A8B2",
-                    "400": "#E46778",
-                    "500": "#D9263E",
-                    "600": "#C32238",
-                    "700": "#821725",
-                    "800": "#62111C",
-                    "900": "#410B13",
-                },
-                "purple": {
-                    "100": "#f3f4fc",
-                    "200": "#d0d1f1",
-                    "300": "#acafe7",
-                    "400": "#898ddc",
-                    "500": "#6167d1",
-                    "600": "#383ebd",
-                    "700": "#292d8a",
-                    "800": "#1b1e5b",
-                    "900": "#0c0d27"
-                }
-            },
             width:        {
                 "content": "max-content",
                 "128":     "32rem"
@@ -176,6 +162,7 @@ module.exports = {
         }
     },
     variants: {
+        fontWeight:  ['hover', 'focus'],
         margin:      ["responsive", "first"],
         borderWidth: ["responsive", "first"],
         aspectRatio: ["responsive"],
