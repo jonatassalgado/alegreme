cf538aefdb8890194a6db2f7ab35d2d70024ca1e
jonatassalgado <jonataseduardosalgado@gmail.com>
2021-01-24 15:29:24 -0300

    Adjust style of calendar in users#calendar

--- web/app/components/calendar_component.rb
+++ web/app/components/calendar_component.rb
@@ -18,7 +18,7 @@ class CalendarComponent < ViewComponent::Base
 	def td_classes_for(day)
 		today = Date.current
 
-		td_class = ['day h-12 text-center px-1 rounded-full cursor-pointer md:hover:bg-green-100 md:hover:text-green-500']
+		td_class = ['day h-14 text-center px-1 rounded-full cursor-pointer md:hover:bg-green-100 md:hover:text-green-500']
 		td_class << "wday-#{day.wday.to_s}"
 		td_class << 'bg-gray-100 rounded-full text-gray-600' if today == day
 		td_class << 'text-gray-300' if today > day
