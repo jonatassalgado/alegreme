dcd1dd8455ac4549b2a9e6eaf8298d994537431a
jonatassalgado <jonataseduardosalgado@gmail.com>
2021-01-31 11:56:17 -0300

    Remove unused npm packages

--- web/app/javascript/modules/pubsub-module.js
+++ web/app/javascript/modules/pubsub-module.js
@@ -1,26 +1,26 @@
-import NanoEvents from "nanoevents";
-
-const PubSubModule = (() => {
-    const debug  = false;
-
-    const nanoEvents = new NanoEvents();
-    if (debug) console.log("[PUBSUB]: started");
-
-    return {
-        destroy: (subscriptions) => {
-            Object.entries(subscriptions).forEach(subscription => {
-                if (typeof subscription[1] == "function") subscription[1]()
-            })
-        },
-        emit:    (e) => {
-            return nanoEvents.emit(e)
-        },
-        on: (e, cb) => {
-            return nanoEvents.on(e, cb)
-        }
-    };
-})();
-
-export {PubSubModule};
-
-
+// import NanoEvents from "nanoevents";
+//
+// const PubSubModule = (() => {
+//     const debug  = false;
+//
+//     const nanoEvents = new NanoEvents();
+//     if (debug) console.log("[PUBSUB]: started");
+//
+//     return {
+//         destroy: (subscriptions) => {
+//             Object.entries(subscriptions).forEach(subscription => {
+//                 if (typeof subscription[1] == "function") subscription[1]()
+//             })
+//         },
+//         emit:    (e) => {
+//             return nanoEvents.emit(e)
+//         },
+//         on: (e, cb) => {
+//             return nanoEvents.on(e, cb)
+//         }
+//     };
+// })();
+//
+// export {PubSubModule};
+//
+//
