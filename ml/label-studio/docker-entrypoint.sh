7ca53c62e4a3c64a8fc2789762717f99ff915f0d
jonatassalgado <jonataseduardosalgado@gmail.com>
2021-03-13 13:48:35 -0300

    Add multiple label annotations with categories and price in label studio

--- ml/label-studio/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Interpreter identifier
-
-set -e
-# Exit on fail
-
-
-exec "$@"
-# Finally call command issued to the docker service
---
+++ ml/label-studio/event-price/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+# Exit on fail
+
+
+
+exec "$@"
