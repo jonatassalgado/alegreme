385c9c6c89d5a012dd3af0562447227b8649e9d7
jonatassalgado <jonataseduardosalgado@gmail.com>
2021-06-11 21:33:10 -0300

    Adjust scrapy settings to be more fast in autothrottle

--- scrapy/alegreme/settings.py
+++ scrapy/alegreme/settings.py
@@ -133,10 +133,10 @@ AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 AUTOTHROTTLE_START_DELAY = 2
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 120
+AUTOTHROTTLE_MAX_DELAY = 30
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 5.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 2.0
 # Enable showing throttling stats for every response received:
 # AUTOTHROTTLE_DEBUG = False
 
