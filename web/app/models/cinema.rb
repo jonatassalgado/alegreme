10bac49be182b226e56bef6a10e442a4178bc277
jonatassalgado <jonataseduardosalgado@gmail.com>
2021-07-11 20:40:42 -0300

    Fix in cinemas group by name when populate

--- web/app/models/cinema.rb
+++ web/app/models/cinema.rb
@@ -2,7 +2,7 @@ class Cinema < ApplicationRecord
 
 	extend FriendlyId
 
-	validates :address, uniqueness: true
+	validates :google_maps, uniqueness: true
 
 	has_many :screenings, dependent: :destroy
 	has_many :movies, -> { distinct }, through: :screenings, dependent: :destroy
