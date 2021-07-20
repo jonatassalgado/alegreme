a750e428682d904b2224474b4adf4f1700642863
jonatassalgado <jonataseduardosalgado@gmail.com>
2021-07-20 12:49:11 -0300

    Fix in search#index style

--- web/app/controllers/search_controller.rb
+++ web/app/controllers/search_controller.rb
@@ -6,16 +6,16 @@ class SearchController < ApplicationController
 		if params[:q]
 			query = params[:q].downcase.split.delete_if { |word| Event::STOPWORDS.include?(word) }.join(' ')
 
-			@search_result  = Event.search(query, {
-				fields:        ["name^2", "organizers", "description", "category"],
-				suggest:       true,
-				limit:         150,
-				includes:      [:place, :organizers, :categories],
-				operator:      "or",
-				body_options:  { min_score: 10 },
-				scope_results: ->(r) { r.active }
-			})
-			# @search_result  = Event.active.not_ml_data.includes(:place).order_by_date
+			# @search_result  = Event.search(query, {
+			# 	fields:        ["name^2", "organizers", "description", "category"],
+			# 	suggest:       true,
+			# 	limit:         150,
+			# 	includes:      [:place, :organizers, :categories],
+			# 	operator:      "or",
+			# 	body_options:  { min_score: 10 },
+			# 	scope_results: ->(r) { r.active }
+			# })
+			@search_result  = Event.active.not_ml_data.includes(:place).order_by_date
 			@founded_events = Event.includes(:place, :organizers, :categories).where(id: @search_result.map(&:id)).not_ml_data
 			@liked_events   = current_user&.liked_events&.not_ml_data&.active&.order_by_date || Event.none
 		else
