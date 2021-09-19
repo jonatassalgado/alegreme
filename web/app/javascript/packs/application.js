fd342b44eb11c5e6835b1d2b2c7675940601998b
jonatassalgado <jonataseduardosalgado@gmail.com>
2021-09-19 18:23:07 -0300

    Remove javacript runtime and core-js from bundle

--- web/app/javascript/packs/application.js
+++ web/app/javascript/packs/application.js
@@ -3,8 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
+// import 'core-js/stable'
+// import 'regenerator-runtime/runtime'
 
 require("@rails/ujs").start()
 // require("turbolinks").start()
